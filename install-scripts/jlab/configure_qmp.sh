5912639595103d654839034a11d0a48b3d0083fc
edwards <edwards@553b7cea-037e-44b4-985c-3430d57e5cd3>
2004-10-18 18:40:26 +0000

    JLab configure script

---
+++ install-scripts/jlab/configure_qmp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../configure --prefix=/usr/local/qmp-2/mpich-gm-1.2.5 --with-qmp-comms-type=mpi --with-qmp-comms-cflags=-I/usr/local/mpich-gm-1.2.5/include --with-qmp-comms-ldflags="-L/usr/local/mpich-gm-1.2.5/lib -L/usr/local/gm-1.6.4/lib" --with-qmp-comms-libs="-lmpich -lgm -lpthread"
