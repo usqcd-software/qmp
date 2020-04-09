1ad99404afcd6120355f9b29f3719341227a8981
Dan H <goracle@users.noreply.github.com>
2020-04-09 12:28:13 -0400

    Add config files to git.

---
+++ include/qmp_config.h
@@ -0,0 +1,56 @@
+/* include/qmp_config.h.  Generated from qmp_config.h.in by configure.  */
+/* include/qmp_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* compiling for BG/L */
+/* #undef HAVE_BGL */
+
+/* compiling for BG/P */
+/* #undef HAVE_BGP */
+
+/* compiling for BG/Q */
+/* #undef HAVE_BGQ */
+
+/* compiling for SPI */
+/* #undef HAVE_BGSPI */
+
+/* compiling for MPI */
+#define HAVE_MPI /**/
+
+/* Name of package */
+#define PACKAGE "qmp"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "detar at physics.utah.edu"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "qmp"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "qmp 2.5.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "qmp"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.5.0"
+
+/* build QMP to allow profiling */
+/* #undef QMP_BUILD_PROFILING */
+
+/* build QMP with function timing */
+/* #undef QMP_BUILD_TIMING */
+
+/* Version number of package */
+#define VERSION "2.5.0"
+
+/* Define if using the dmalloc debugging malloc package */
+/* #undef WITH_DMALLOC */
+
+/* enable extra debugging */
+/* #undef _QMP_DEBUG */
+
+/* enable extra tracing */
+/* #undef _QMP_TRACE */
